a1752bc7dcaf5c5c0c2b4c61d0a1a88ce920a8d9
Yegor <nataavetisyann@gmail.com>
2024-10-10 21:34:42 +0300

    categories conection

--- src/ListGroup/Categories.js
+++ src/ListGroup/Categories.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const fetchCategories = async () => {
     try{
-        var response = await axios.get("http://localhost:5257/api/categories");
+        var response = await axios.get("https://avet-shop-748665ae765c.herokuapp.com/api/categories");
         console.log(response.data.categories);
         return response.data.categories;
     }catch(e){
